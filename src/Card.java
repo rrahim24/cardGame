8fde819e1d9cdcaaebd9dd9bfd6fefda76169a9b
rayanrahim <rayan.rahim@menloschool.org>
2023-11-27 13:49:06 -0800

    New constructor for Card class

--- src/Card.java
+++ src/Card.java
@@ -2,4 +2,9 @@ public class Card {
     private int point;
     private String suit;
     private String rank;
+
+    public Card(String suit, String rank){
+        this.suit = suit;
+        this.rank = rank;
+    }
 }
